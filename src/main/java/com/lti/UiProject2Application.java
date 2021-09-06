f7b85483b0ab880210681006d5723323f6f01381
Sanjaymutkule <sanjaymutkule8@gmail.com>
2021-09-06 19:33:11 +0530

    This is second Commit

---
+++ src/main/java/com/lti/UiProject2Application.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiProject2Application.class, args);
+	}
+
+}
